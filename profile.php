eb650988aed6b9d500481980d0fb519946fc64fe
felpsilva <felipep.silva5741@hotmail.com>
2024-03-18 00:47:01 -0300

    Corrigindo variável

--- profile.php
+++ profile.php
@@ -1,10 +1,10 @@
 <?php
-require_once("template/header.php");
+require_once ("template/header.php");
 
 //Verifica se usuário está autenticado
-require_once("models/User.php");
-require_once("dao/UserDAO.php");
-require_once("dao/MovieDAO.php");
+require_once ("models/User.php");
+require_once ("dao/UserDAO.php");
+require_once ("dao/MovieDAO.php");
 
 $user = new User();
 $userDao = new UserDAO($conn, $BASE_URL);
@@ -13,8 +13,8 @@ $movieDao = new MovieDAO($conn, $BASE_URL);
 //Receber id do usuário
 $id = filter_input(INPUT_GET, "id");
 
-if (empty($id)) {
-  if (!empty($userData)) {
+if (empty ($id)) {
+  if (!empty ($userData)) {
     $id = $userData->id;
   } else {
     $message->setMessage("Usuário não encontrado!", "error", "index.php");
@@ -22,7 +22,7 @@ if (empty($id)) {
   }
 } else {
 
-  $userData = $userDAO->findById($id);
+  $userData = $userDao->findById($id);
 
   //Se não encontrar usuário
   if (!$userData) {
@@ -49,7 +49,7 @@ $userMovies = $movieDao->getMoviesByUserId($id);
         <div id="profile-image-container" class="profile-image"
           style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>')"></div>
         <h3 class="about-title">Sobre: </h3>
-        <?php if (!empty($userData->bio)): ?>
+        <?php if (!empty ($userData->bio)): ?>
           <p class="profile-description">
             <?= $userData->bio ?>
           </p>
@@ -60,17 +60,17 @@ $userMovies = $movieDao->getMoviesByUserId($id);
       <div class="col-md-12 added-movies-container">
         <h3>Filmes que enviou: </h3>
         <div class="movies-container">
-        <?php foreach ($userMovies as $movie): ?>
-          <?php require("template/movie_card.php") ?>
-        <?php endforeach; ?>
-        <?php if (count($userMovies) === 0): ?>
-          <p class="empty-list">O usuário ainda não enviou filmes.</p>
-        <?php endif; ?>
+          <?php foreach ($userMovies as $movie): ?>
+            <?php require ("template/movie_card.php") ?>
+          <?php endforeach; ?>
+          <?php if (count($userMovies) === 0): ?>
+            <p class="empty-list">O usuário ainda não enviou filmes.</p>
+          <?php endif; ?>
         </div>
       </div>
     </div>
   </div>
 </div>
 <?php
-require_once("template/footer.php")
+require_once ("template/footer.php")
   ?>
